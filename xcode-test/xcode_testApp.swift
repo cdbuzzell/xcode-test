f7ff2570d61915f8b39f3e42709e2ec2a3682362
Corey Buzzell <cdbuzzell@gmail.com>
2021-02-07 08:39:54 -0600

    Initial Commit

---
+++ xcode-test/xcode_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_testApp.swift
+//  xcode-test
+//
+//  Created by Corey Buzzell on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct xcode_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
